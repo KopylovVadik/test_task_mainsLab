3b4efd66b5793b398a5abda41c38cb12b9e655e1
vadim.kopylov <vadim.kopylov@osinit.com>
2022-07-01 18:53:01 +0300

    added docker discription

--- mains_lab_api/settings.py
+++ mains_lab_api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-2forrx6lha8(^e_6amfc9&-n3&bq=ah*9tz0dzhxw7_bv+_21(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
